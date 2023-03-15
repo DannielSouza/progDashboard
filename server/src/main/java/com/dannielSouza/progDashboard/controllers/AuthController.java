d4792ff5fdbae8b17a7ebdca8807be308d8d3334
Daniel  Souza <104663666+DannielSouza@users.noreply.github.com>
2023-03-15 20:03:00 -0300

    authUser response update

--- server/src/main/java/com/dannielSouza/progDashboard/controllers/AuthController.java
+++ server/src/main/java/com/dannielSouza/progDashboard/controllers/AuthController.java
@@ -2,14 +2,10 @@ package com.dannielSouza.progDashboard.controllers;
 
 import com.dannielSouza.progDashboard.models.Company;
 import com.dannielSouza.progDashboard.services.AuthService;
-import com.dannielSouza.progDashboard.services.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -23,7 +19,9 @@ public class AuthController {
 
     @PostMapping
     @CrossOrigin("http://localhost:3000")
-    public ResponseEntity<Map<String, String>> authUser(@RequestBody String token){
-        return service.autoAutheticate(token);
+    public ResponseEntity<Map<String, String>> authUser(@RequestHeader Map<String, String> header){
+        String token = header.get("token");
+        Map<String, String> verified = service.autoAutheticate(token);
+        return ResponseEntity.ok().body(verified);
     }
 }
