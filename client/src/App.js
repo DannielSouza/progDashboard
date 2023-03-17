bf142e5aeffe6dc9ac5a179f70519e02dcc8dcff
Daniel  Souza <104663666+DannielSouza@users.noreply.github.com>
2023-03-17 19:32:50 -0300

    company header added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CompanyAuth from "./components/pages/CompanyAuth";
 import CompanyHome from "./components/pages/CompanyHome";
 import SelectAccount from "./components/pages/SelectAccount";
 import UserAuth from "./components/pages/UserAuth";
+import { useSelector } from 'react-redux'
+import CompanyHeader from "./components/CompanyHeader";
 
 function App() {
+  const { currentUser } = useSelector(rootReducer => rootReducer)
+  const [enableCompanyHeader, setEnableCompanyHeader] = React.useState(false)
+
+  React.useEffect(()=>{
+    if(currentUser && currentUser.role === "ADMIN") setEnableCompanyHeader(true)
+  },[currentUser])
+
   return (
     <BrowserRouter>
+        {enableCompanyHeader && <CompanyHeader />}
       <Routes>
-
+        
         <Route path="/" element={<SelectAccount />} />
         <Route path="/userAuth" element={<UserAuth />} />
         <Route path="/companyAuth" element={<CompanyAuth />} />
